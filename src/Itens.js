d4a389a8077291984dd2af8ca14422de84350aff
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-21 16:30:15 -0300

    axios installed

--- src/Itens.js
+++ src/Itens.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react'
 import {Text, StyleSheet} from 'react-native'
+import axios from 'axios'
 
 export default class Itens extends Component {
+
+  componentWillMount(){
+    return axios
+      .get('https://jsonplaceholder.typicode.com/photos')
+      .then(response => {console.log(response)})
+      .catch(error => console.log(error))
+  }
+
   render(){
     return (
      <Text style={Estilo.teste}>Itens</Text>
